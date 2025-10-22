b84b88e51b96a05b1491ed3720d1300fab61e8b1
hleopary <cacholeo@gmail.com>
2025-10-22 13:06:50 -0300

    corrigiendo Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useCartContext } from "../../context/CartContext/CartContext";
+import { useCartContext } from "../context/CartContext/CartContext";
 
 
 export const Nav = () => {
